abe570298608d36b8240bd4d6f226670a8ad575e
kyoxd1 <kyomaster1234@gmail.com>
2022-04-17 19:03:28 -0400

    findArticlePerId

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get '', to: "home#index"
 
   get 'articles/new', to: "articles#new"
+  get 'articles/:id', to: "articles#show"
+
   post 'articles', to: "articles#create"
 
 end
