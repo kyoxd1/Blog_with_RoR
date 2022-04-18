88653435a5e5c97554fc29c39d93af0d7d21e243
kyoxd1 <kyomaster1234@gmail.com>
2022-04-18 19:01:35 -0400

    ModelUserCreatedAndAuthentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 
     before_action :find_article, only: [:edit, :update, :show, :destory]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destory]
 
     def index
         @articles = Article.all
