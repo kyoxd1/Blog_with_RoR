e618f08511b7bc30692681f172860213e791a1e3
kyoxd1 <kyomaster1234@gmail.com>
2022-04-23 22:00:41 -0400

    RelationManyToManyBetweenCategoriesAndArticles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
     has_rich_text :content
     belongs_to :user
+    has_many :category_articles
+    has_many :categories, through: :category_articles
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_24_003849) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_24_015438) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -66,6 +66,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_24_003849) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "category_articles", force: :cascade do |t|
+    t.integer "article_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_category_articles_on_article_id"
+    t.index ["category_id"], name: "index_category_articles_on_category_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -83,4 +92,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_24_003849) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "articles", "users"
+  add_foreign_key "category_articles", "articles"
+  add_foreign_key "category_articles", "categories"
 end
